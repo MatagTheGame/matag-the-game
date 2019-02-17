884eacf0d7c527fe29d6a75702867ab80397bc92
Antonio Alonzi <antonioalonzi85@gmail.com>
2019-02-17 19:41:02 +0000

    Identify the active player.

--- src/main/resources/app/src/mtg/PlayerInfo/OpponentInfo.js
+++ src/main/resources/app/src/mtg/PlayerInfo/OpponentInfo.js
@@ -4,8 +4,16 @@ import {get} from 'lodash'
 
 class OpponentInfo extends PureComponent {
   render() {
+    let classes = 'player-info'
+
+    if (this.props.currentTurnPlayer === this.props.name) {
+      classes += ' active-player'
+    } else {
+      classes += ' inactive-player'
+    }
+
     return (
-      <div id="opponent-info" className='player-info'>
+      <div id="opponent-info" className={classes}>
         <span>{this.props.name}</span>
         <span>{this.props.life}</span>
       </div>
@@ -16,7 +24,8 @@ class OpponentInfo extends PureComponent {
 const mapStateToProps = state => {
   return {
     name: get(state, 'opponent.name', ''),
-    life: get(state, 'opponent.life', '')
+    life: get(state, 'opponent.life', ''),
+    currentTurnPlayer: get(state, 'turn.status.currentTurnPlayer')
   }
 }
 
--- src/main/resources/app/src/mtg/PlayerInfo/PlayerInfo.js
+++ src/main/resources/app/src/mtg/PlayerInfo/PlayerInfo.js
@@ -4,8 +4,16 @@ import {get} from 'lodash'
 
 class PlayerInfo extends PureComponent {
   render() {
+    let classes = 'player-info'
+
+    if (this.props.currentTurnPlayer === this.props.name) {
+      classes += ' active-player'
+    } else {
+      classes += ' inactive-player'
+    }
+
     return (
-      <div id="player-info" className='player-info'>
+      <div id="player-info" className={classes}>
         <span>{this.props.name}</span>
         <span>{this.props.life}</span>
       </div>
@@ -16,7 +24,8 @@ class PlayerInfo extends PureComponent {
 const mapStateToProps = state => {
   return {
     name: get(state, 'player.name', ''),
-    life: get(state, 'player.life', '')
+    life: get(state, 'player.life', ''),
+    currentTurnPlayer: get(state, 'turn.status.currentTurnPlayer')
   }
 }
 
