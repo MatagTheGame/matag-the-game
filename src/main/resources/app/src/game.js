f9bc929befeb49dd9ddb9852a5edb9dac9181713
Antonio Alonzi <antonioalonzi85@gmail.com>
2019-02-24 12:33:06 +0000

    Send PLAY_LAND event

--- src/main/resources/app/src/game.js
+++ src/main/resources/app/src/game.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import App from './game/App'
 import AppReducer from './game/_reducers/AppReducer'
 
-const store = createStore(AppReducer, composeWithDevTools());
+const store = createStore(AppReducer, composeWithDevTools())
 
 render(
   <Provider store={store}>
--- src/main/resources/app/src/game/App.js
+++ src/main/resources/app/src/game/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {Fragment, PureComponent} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import stompClient from './WebSocket'
