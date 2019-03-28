0f45545ddcefa06577521346734dfcdc836a4db4
Antonio Alonzi <antonioalonzi85@gmail.com>
2019-03-28 23:04:24 +0000

    WB and RG decks.

--- src/main/java/com/aa/mtg/cards/sets/RavnicaAllegiance.java
+++ src/main/java/com/aa/mtg/cards/sets/RavnicaAllegiance.java
@@ -10,7 +10,10 @@ import static java.util.Collections.singletonList;
 
 public class RavnicaAllegiance {
 
-    public static Card FERAL_MAAKA = new Card("Feral Maaka", singletonList(Color.RED), asList(Cost.RED, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Cat"), "", 2, 2);
     public static Card AXEBANE_BEAST = new Card("Axebane Beast", singletonList(Color.GREEN), asList(Cost.GREEN, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Beast"), "", 3, 4);
+    public static Card CATACOMB_CROCODILE = new Card("Catacomb Crocodile", singletonList(Color.BLACK), asList(Cost.BLACK, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Crocodile"), "", 3, 7);
+    public static Card CORAL_COMMANDO = new Card("Coral Commando", singletonList(Color.BLUE), asList(Cost.BLUE, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), asList("Merfolk", "Warrior"), "", 3, 2);
+    public static Card FERAL_MAAKA = new Card("Feral Maaka", singletonList(Color.RED), asList(Cost.RED, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Cat"), "", 2, 2);
+    public static Card PROWLING_CARACAL = new Card("Prowling Caracal", singletonList(Color.WHITE), asList(Cost.WHITE, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Cat"), "", 3, 1);
 
 }
--- src/main/java/com/aa/mtg/game/deck/DeckRetrieverService.java
+++ src/main/java/com/aa/mtg/game/deck/DeckRetrieverService.java
@@ -3,40 +3,57 @@ package com.aa.mtg.game.deck;
 import com.aa.mtg.cards.Card;
 import com.aa.mtg.cards.CardInstance;
 import com.aa.mtg.game.player.Library;
-import com.aa.mtg.game.status.GameStatus;
 import com.aa.mtg.game.security.SecurityToken;
+import com.aa.mtg.game.status.GameStatus;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static com.aa.mtg.cards.Cards.FOREST;
 import static com.aa.mtg.cards.Cards.MOUNTAIN;
+import static com.aa.mtg.cards.Cards.PLAINS;
+import static com.aa.mtg.cards.Cards.SWAMP;
 import static com.aa.mtg.cards.sets.RavnicaAllegiance.AXEBANE_BEAST;
+import static com.aa.mtg.cards.sets.RavnicaAllegiance.CATACOMB_CROCODILE;
 import static com.aa.mtg.cards.sets.RavnicaAllegiance.FERAL_MAAKA;
+import static com.aa.mtg.cards.sets.RavnicaAllegiance.PROWLING_CARACAL;
 
 @Component
 public class DeckRetrieverService {
 
     public Library retrieveDeckForUser(SecurityToken token, String playerName, GameStatus gameStatus) {
-        List<CardInstance> libraryCards = IntStream.rangeClosed(1, 60)
-                .boxed()
-                .map(i -> new CardInstance(gameStatus.nextCardId(), getRandomCard(), playerName))
-                .collect(Collectors.toList());
-
-        return new Library(libraryCards);
+        if (playerName.equals("Pippo")) {
+            return pippoDeck(playerName, gameStatus);
+        } else {
+            return plutoDeck(playerName, gameStatus);
+        }
     }
 
-    private Card getRandomCard() {
-        switch (new Random().nextInt(4)) {
-            case 0: return MOUNTAIN;
-            case 1: return FOREST;
-            case 2: return FERAL_MAAKA;
-            case 3: return AXEBANE_BEAST;
+    private Library pippoDeck(String playerName, GameStatus gameStatus) {
+        return createDeck(playerName, gameStatus, SWAMP, PLAINS, CATACOMB_CROCODILE, PROWLING_CARACAL);
+    }
+
+    private Library plutoDeck(String playerName, GameStatus gameStatus) {
+        return createDeck(playerName, gameStatus, MOUNTAIN, FOREST, FERAL_MAAKA, AXEBANE_BEAST);
+    }
+
+    private Library createDeck(String playerName, GameStatus gameStatus, Card land1, Card land2, Card creature1, Card creature2) {
+        List<CardInstance> cards = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            cards.add(new CardInstance(gameStatus.nextCardId(), land1, playerName));
+            cards.add(new CardInstance(gameStatus.nextCardId(), land2, playerName));
         }
-        throw new RuntimeException("Non Existent Card");
+
+        for (int i = 0; i < 18; i++) {
+            cards.add(new CardInstance(gameStatus.nextCardId(), creature1, playerName));
+            cards.add(new CardInstance(gameStatus.nextCardId(), creature2, playerName));
+        }
+
+        Collections.shuffle(cards);
+        return new Library(cards);
     }
 
 }
