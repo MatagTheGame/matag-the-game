f25bf7f6e3aa8addb146536c6eb45920c003ad34
Antonio Alonzi <antonioalonzi85@gmail.com>
2019-06-26 18:38:09 +0100

    Test for: Fixed needsTargets looking if the cards has abilities.

--- src/test/java/application/cast/CastCreatureTest.java
+++ src/test/java/application/cast/CastCreatureTest.java
@@ -16,7 +16,7 @@ import static application.browser.BattlefieldHelper.FIRST_LINE;
 import static application.browser.BattlefieldHelper.SECOND_LINE;
 import static com.aa.mtg.cards.Cards.ISLAND;
 import static com.aa.mtg.cards.Cards.MOUNTAIN;
-import static com.aa.mtg.cards.sets.Ixalan.AIR_ELEMENTAL;
+import static com.aa.mtg.cards.sets.Ixalan.HEADWATER_SENTRIES;
 import static com.aa.mtg.game.player.PlayerType.OPPONENT;
 import static com.aa.mtg.game.player.PlayerType.PLAYER;
 import static com.aa.mtg.game.turn.phases.Main1Phase.M1;
@@ -28,7 +28,7 @@ public class CastCreatureTest extends AbstractApplicationTest {
     @Test
     public void castCreature() {
         // When click on creature without paying the cost
-        browser.player1().getHandHelper(PLAYER).getFirstCard(AIR_ELEMENTAL).click();
+        browser.player1().getHandHelper(PLAYER).getFirstCard(HEADWATER_SENTRIES).click();
 
         // Then stack is still empty
         browser.player1().getStackHelper().toHaveSize(0);
@@ -48,7 +48,7 @@ public class CastCreatureTest extends AbstractApplicationTest {
         browser.player1().getBattlefieldHelper(PLAYER, FIRST_LINE).getCard(MOUNTAIN, 1).isFrontendTapped();
 
         // When click on creature
-        browser.player1().getHandHelper(PLAYER).getCard(AIR_ELEMENTAL, 0).click();
+        browser.player1().getHandHelper(PLAYER).getCard(HEADWATER_SENTRIES, 0).click();
 
         // Then all lands are tapped for both players
         browser.player1().getBattlefieldHelper(PLAYER, FIRST_LINE).getCard(ISLAND, 0).isTapped();
@@ -64,9 +64,9 @@ public class CastCreatureTest extends AbstractApplicationTest {
         browser.player2().getBattlefieldHelper(OPPONENT, FIRST_LINE).getCard(MOUNTAIN, 1).isTapped();
 
         // And creature is on the stack for both players (hands is empty)
-        browser.player1().getStackHelper().containsExactly(AIR_ELEMENTAL);
+        browser.player1().getStackHelper().containsExactly(HEADWATER_SENTRIES);
         browser.player1().getHandHelper(PLAYER).isEmpty();
-        browser.player2().getStackHelper().containsExactly(AIR_ELEMENTAL);
+        browser.player2().getStackHelper().containsExactly(HEADWATER_SENTRIES);
         browser.player1().getHandHelper(OPPONENT).isEmpty();
 
         // And priority is to opponent with corresponding status
@@ -81,8 +81,8 @@ public class CastCreatureTest extends AbstractApplicationTest {
         browser.player2().getActionHelper().clickContinue();
 
         // Creature goes on the battlefield (stack is empty)
-        browser.player1().getBattlefieldHelper(PLAYER, SECOND_LINE).contains(AIR_ELEMENTAL);
-        browser.player2().getBattlefieldHelper(OPPONENT, SECOND_LINE).contains(AIR_ELEMENTAL);
+        browser.player1().getBattlefieldHelper(PLAYER, SECOND_LINE).contains(HEADWATER_SENTRIES);
+        browser.player2().getBattlefieldHelper(OPPONENT, SECOND_LINE).contains(HEADWATER_SENTRIES);
         browser.player1().getStackHelper().isEmpty();
         browser.player2().getStackHelper().isEmpty();
 
@@ -106,7 +106,7 @@ public class CastCreatureTest extends AbstractApplicationTest {
             return new InitTestService() {
                 @Override
                 protected void initGameStatus(GameStatus gameStatus) {
-                    addCardToCurrentPlayerHand(gameStatus, AIR_ELEMENTAL);
+                    addCardToCurrentPlayerHand(gameStatus, HEADWATER_SENTRIES);
                     addCardToCurrentPlayerBattlefield(gameStatus, ISLAND);
                     addCardToCurrentPlayerBattlefield(gameStatus, ISLAND);
                     addCardToCurrentPlayerBattlefield(gameStatus, ISLAND);
