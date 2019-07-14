49b241cc64134eabcbc22dcc5371b78dec074ff9
Antonio Alonzi <antonioalonzi85@gmail.com>
2019-07-14 13:34:44 -0700

    Added more creatures.

--- src/main/java/com/aa/mtg/cards/sets/CoreSet2020.java
+++ src/main/java/com/aa/mtg/cards/sets/CoreSet2020.java
@@ -25,6 +25,10 @@ public class CoreSet2020 implements MtgSet {
     public static Card BOGSTOMPER = new Card("Bogstomper", singletonList(Color.BLACK), asList(Cost.BLACK, Cost.BLACK, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Beast"), "", 6, 5, emptyList());
     public static Card CANOPY_SPIDER = new Card("Canopy Spider", singletonList(Color.GREEN), asList(Cost.GREEN, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Spider"), "", 1, 3, singletonList(REACH));
     public static Card CENTAUR_COURSER = new Card("Centaur Courser", singletonList(Color.GREEN), asList(Cost.GREEN, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), asList("Centaur", "Warrior"), "", 3, 3, emptyList());
+    public static Card CLOUDKIN_SEER = new Card("Cloudkin Seer", singletonList(Color.BLUE), asList(Cost.BLUE, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), asList("Elemental", "Wizard"), "Flying. When Cloudkin Seer enters the battlefield, draw a card.", 2, 1, singletonList(WHEN_IT_ENTERS_THE_BATTLEFIELD_DRAW_A_CARD));
+    public static Card CORAL_MERFOLK = new Card("Coral Merfolk", singletonList(Color.BLUE), asList(Cost.BLUE, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Merfolk"), "", 2, 1, emptyList());
+    public static Card DAWNING_ANGEL = new Card("Dawning Angel", singletonList(Color.WHITE), asList(Cost.WHITE, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Angel"), "Flying. When Dawning Angel enters the battlefield, you gain 4 life.", 3, 2, asList(FLYING, WHEN_IT_ENTERS_THE_BATTLEFIELD_GAIN_4_LIFE));
+    public static Card DAYBREAK_CHAPLAIN = new Card("Daybreak Chaplain", singletonList(Color.WHITE), asList(Cost.WHITE, Cost.COLORLESS), singletonList(Type.CREATURE), asList("Human", "Cleric"), "Lifelink", 1, 3, singletonList(LIFELINK));
     public static Card MARAUDERS_AXE = new Card("Marauder's Axe", emptyList(), asList(Cost.COLORLESS, Cost.COLORLESS), singletonList(ARTIFACT), singletonList("Equipment"), "Equipped creature gets +2/+0. Equip 2", 0, 0, singletonList(PAY_2_EQUIP_CREATURE_GETS_PLUS_2_0));
     public static Card OAKENFORM = new Card("Oakenform", singletonList(Color.GREEN), asList(Cost.GREEN, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.ENCHANTMENT), singletonList("Aura"), "Enchant creature. Enchanted creature gets +3/+3.", 0, 0, singletonList(ENCHANTED_CREATURE_GETS_PLUS_3_3));
 
@@ -40,6 +44,10 @@ public class CoreSet2020 implements MtgSet {
         cards.add(BOGSTOMPER);
         cards.add(CANOPY_SPIDER);
         cards.add(CENTAUR_COURSER);
+        cards.add(CLOUDKIN_SEER);
+        cards.add(CORAL_MERFOLK);
+        cards.add(DAWNING_ANGEL);
+        cards.add(DAYBREAK_CHAPLAIN);
         cards.add(MARAUDERS_AXE);
         cards.add(OAKENFORM);
     }
--- src/main/java/com/aa/mtg/cards/sets/GuildsOfRavnica.java
+++ src/main/java/com/aa/mtg/cards/sets/GuildsOfRavnica.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.aa.mtg.cards.ability.Abilities.ENCHANTED_CREATURE_GETS_PLUS_3_2_AND_VIGILANCE;
+import static com.aa.mtg.cards.ability.Abilities.LIFELINK;
 import static com.aa.mtg.cards.ability.Abilities.WHEN_IT_ENTERS_THE_BATTLEFIELD_EACH_PLAYERS_GAIN_4_LIFE;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -19,6 +20,7 @@ public class GuildsOfRavnica implements MtgSet {
 
     public static Card CANDLELIGHT_VIGIL = new Card("Candlelight Vigil", singletonList(Color.WHITE), asList(Cost.WHITE, Cost.COLORLESS, Cost.COLORLESS, Cost.COLORLESS), singletonList(Type.ENCHANTMENT), singletonList("Aura"), "Enchant Creature. Enchanted creature gets +3/+2 and has vigilance.", 0, 0, singletonList(ENCHANTED_CREATURE_GETS_PLUS_3_2_AND_VIGILANCE));
     public static Card CENTAUR_PEACEMAKER = new Card("Centaur Peacemaker", asList(Color.WHITE, Color.GREEN), asList(Cost.WHITE, Cost.GREEN, Cost.COLORLESS), singletonList(Type.CREATURE), asList("Centaur", "Cleric"), "When Centaur Peacemaker enters the battlefield, each player gains 4 life.", 3, 3, singletonList(WHEN_IT_ENTERS_THE_BATTLEFIELD_EACH_PLAYERS_GAIN_4_LIFE));
+    public static Card CHILD_OF_NIGHT = new Card("Child of Night", singletonList(Color.BLACK), asList(Cost.BLACK, Cost.COLORLESS), singletonList(Type.CREATURE), singletonList("Vampire"), "Lifelink", 2, 1, singletonList(LIFELINK));
 
     private static GuildsOfRavnica instance;
 
@@ -26,6 +28,8 @@ public class GuildsOfRavnica implements MtgSet {
 
     private GuildsOfRavnica() {
         cards.add(CANDLELIGHT_VIGIL);
+        cards.add(CENTAUR_PEACEMAKER);
+        cards.add(CHILD_OF_NIGHT);
     }
 
     @Override
