238b0a68fedcde98d785afc2678f786f1a98a8a5
Antonio Alonzi <aalonzi@ebay.com>
2019-10-27 17:41:14 +0000

    Added admin module.

---
+++ admin/src/main/java/com/aa/mtg/admin/TestController.java
@@ -0,0 +1,13 @@
+package com.aa.mtg.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Admin is running.";
+    }
+}
