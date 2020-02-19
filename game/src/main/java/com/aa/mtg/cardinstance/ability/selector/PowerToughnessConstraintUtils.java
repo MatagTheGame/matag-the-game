35088ecb39c86540ff1dd2a9e1aca498aaad66df
Antonio Alonzi <aalonzi@ebay.com>
2020-02-19 19:22:41 +0000

    Added PowerToughnessConstraintUtils.

---
+++ game/src/main/java/com/aa/mtg/cardinstance/ability/selector/PowerToughnessConstraintUtils.java
@@ -0,0 +1,41 @@
+package com.aa.mtg.cardinstance.ability.selector;
+
+import com.aa.mtg.cardinstance.CardInstance;
+import com.aa.mtg.cards.selector.PowerToughnessConstraint;
+
+public class PowerToughnessConstraintUtils {
+    public static boolean check(PowerToughnessConstraint powerToughnessConstraint, CardInstance cardInstance) {
+        int value = getValue(powerToughnessConstraint.getPowerOrToughness(), cardInstance);
+
+        switch (powerToughnessConstraint.getPowerToughnessConstraintType()) {
+            case EQUAL:
+                return value == powerToughnessConstraint.getValue();
+
+            case LESS:
+                return value < powerToughnessConstraint.getValue();
+
+            case LESS_OR_EQUAL:
+                return value <= powerToughnessConstraint.getValue();
+
+            case GREATER:
+                return value > powerToughnessConstraint.getValue();
+
+            case GREATER_OR_EQUAL:
+                return value >= powerToughnessConstraint.getValue();
+
+            default:
+                throw new RuntimeException("powerToughnessConstraintType is null");
+        }
+    }
+
+    private static int getValue(PowerToughnessConstraint.PowerOrToughness powerOrToughness, CardInstance cardInstance) {
+        switch (powerOrToughness) {
+            case POWER:
+                return cardInstance.getPower();
+            case TOUGHNESS:
+                return cardInstance.getToughness();
+            default:
+                throw new RuntimeException("powerOrToughness is null");
+        }
+    }
+}
