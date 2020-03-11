0fb9687123af8f02b7480742e39ad0eecea84bfa
Antonio Alonzi <aalonzi@ebay.com>
2020-03-11 20:20:04 +0000

    Added react router.

--- admin/src/main/resources/app/src/admin/App.js
+++ admin/src/main/resources/app/src/admin/App.js
@@ -1,29 +1,54 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 
 class App extends Component {
-  handleSubmit() {
-    console.log('Login')
-  }
-
   render() {
     return (
-      <div>
-        <h2>Login</h2>
-        <form className="mtg-form" onSubmit={this.handleSubmit()}>
-          <div className="grid grid-label-value">
-            <label htmlFor='username'>Username: </label><input type='text' name='username'/>
-          </div>
-          <div className="grid grid-label-value">
-            <label htmlFor='password'>Password: </label><input type='password' name='password'/>
-          </div>
-          <div className="grid">
-            <input type='submit' value='Login'/>
-          </div>
-        </form>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/ui/admin">Home</Link>
+              </li>
+              <li>
+                <Link to="/ui/admin/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/ui/admin/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/ui/admin/login">
+              <div>
+                <h2>Login</h2>
+                <form className="mtg-form">
+                  <div className="grid grid-label-value">
+                    <label htmlFor='username'>Username: </label><input type='text' name='username'/>
+                  </div>
+                  <div className="grid grid-label-value">
+                    <label htmlFor='password'>Password: </label><input type='password' name='password'/>
+                  </div>
+                  <div className="grid">
+                    <input type='submit' value='Login'/>
+                  </div>
+                </form>
+              </div>
+            </Route>
+            <Route path="/ui/admin/users">
+              <div>Users</div>
+            </Route>
+            <Route path="/ui/admin">
+              <div>Home</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
