5e7aac58128ceb2291e3b4ac070bd8ec197ae6a5
Antonio Alonzi <aalonzi@ebay.com>
2020-04-10 21:35:28 +0100

    Passed token to game.

--- game/src/main/java/com/matag/game/launcher/LauncherGameController.java
+++ game/src/main/java/com/matag/game/launcher/LauncherGameController.java
@@ -4,21 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class LauncherGameController {
-  @Deprecated
-  @RequestMapping(path = {"/ui/game/{id}"}, method = RequestMethod.GET)
-  public String launchGameOld(@PathVariable("id") Long id, HttpServletRequest httpServletRequest) {
-    System.out.println("ciao");
-    return "forward:/game.html";
+  @ResponseBody
+  @RequestMapping(path = {"/ui/game/{id}"}, method = RequestMethod.POST)
+  public String launchGame(@PathVariable("id") Long id, HttpServletRequest httpServletRequest, Map<String, Object> model) {
+    String session = httpServletRequest.getParameter("session");
+    validateSessionFormat(session);
+
+    return "<html>\n" +
+      "    <head>\n" +
+      "        <meta charset=\"utf-8\">\n" +
+      "        <title>MATAG - Game</title>\n" +
+      "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+      "        <link rel=\"shortcut icon\" type=\"image/png\" href=\"/img/matag.png\"/>\n" +
+      "        <script>sessionStorage.setItem('token', '" + session + "')</script>" +
+      "    </head>\n" +
+      "    <body>\n" +
+      "        <div id=\"app\" ></div>\n" +
+      "        <script src=\"/js/game.js\" type=\"text/javascript\"></script>\n" +
+      "    </body>\n" +
+      "</html>";
   }
 
-  @RequestMapping(path = {"/ui/game/{id}"}, method = RequestMethod.POST)
-  public String launchGame(@PathVariable("id") Long id, HttpServletRequest httpServletRequest) {
-    String session = httpServletRequest.getParameter("session");
-    return "forward:/game.html";
+  private void validateSessionFormat(String session) {
+    UUID.fromString(session);
   }
 }
