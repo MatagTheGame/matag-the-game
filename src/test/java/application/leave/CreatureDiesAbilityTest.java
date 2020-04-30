8b133621d52762337e24a394108d8bf2bac4a41b
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-04-30 20:47:10 +0100

    Fixed flaky test.

--- src/test/java/application/leave/CreatureDiesAbilityTest.java
+++ src/test/java/application/leave/CreatureDiesAbilityTest.java
@@ -33,20 +33,24 @@ public class CreatureDiesAbilityTest extends AbstractApplicationTest {
 
     // When opponent kills 1 goblin
     browser.player1().getActionHelper().clickContinue();
+    browser.player2().getPhaseHelper().is("BC", PLAYER);
     browser.player2().getBattlefieldHelper(PLAYER, FIRST_LINE).getCard(cards.get("Swamp"), 0).tap();
     browser.player2().getBattlefieldHelper(PLAYER, FIRST_LINE).getCard(cards.get("Swamp"), 1).tap();
     browser.player2().getBattlefieldHelper(PLAYER, FIRST_LINE).getCard(cards.get("Swamp"), 2).tap();
     browser.player2().getHandHelper(PLAYER).getFirstCard(cards.get("Murder")).click();
     browser.player2().getBattlefieldHelper(OPPONENT, SECOND_LINE).getFirstCard(cards.get("Goblin Assault Team")).click();
     browser.player1().getActionHelper().clickContinue();
+    browser.player1().getPhaseHelper().is("BC", OPPONENT);
 
     // Then put +1/+1 counter is triggered
     browser.player1().getStackHelper().containsAbilitiesExactly(singletonList("Player1's Goblin Assault Team (" + firstGoblinId + "): That targets get 1 +1/+1 counters."));
 
     // When clicking on the other goblin
     browser.player2().getActionHelper().clickContinue();
+    browser.player1().getPhaseHelper().is("BC", PLAYER);
     browser.player1().getBattlefieldHelper(PLAYER, SECOND_LINE).getFirstCard(cards.get("Goblin Assault Team")).target();
     browser.player2().getActionHelper().clickContinue();
+    browser.player1().getPhaseHelper().is("BC", PLAYER);
 
     // Then that goblin gets a counter
     browser.player1().getBattlefieldHelper(PLAYER, SECOND_LINE).getFirstCard(cards.get("Goblin Assault Team")).hasPlus1Counters(1);
