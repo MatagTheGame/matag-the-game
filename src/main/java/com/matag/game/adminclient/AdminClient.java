1b5efde92d2cab9d05037518ccac48670d48eb74
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-04-30 20:37:54 +0100

    Ugly but working fix for AdminClient.

--- src/main/java/com/matag/game/adminclient/AdminClient.java
+++ src/main/java/com/matag/game/adminclient/AdminClient.java
@@ -1,5 +1,7 @@
 package com.matag.game.adminclient;
 
+import static java.util.Collections.singletonList;
+
 import com.matag.adminentities.DeckInfo;
 import com.matag.adminentities.FinishGameRequest;
 import com.matag.adminentities.PlayerInfo;
@@ -10,11 +12,13 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import static java.util.Collections.singletonList;
 
 @Profile("!test")
 @AllArgsConstructor
@@ -34,7 +38,7 @@ public class AdminClient {
 
   public void finishGame(String gameId, Player winner) {
     try {
-      post(winner.getToken(), "/game/" + gameId + "/finish", new FinishGameRequest(winner.getToken().getAdminToken()), Object.class);
+      adminPost("/game/" + gameId + "/finish", new FinishGameRequest(winner.getToken().getAdminToken()), Object.class);
     } catch (Exception e) {
       LOGGER.error("Could not finish the game.", e);
     }
@@ -44,16 +48,28 @@ public class AdminClient {
     return exchange(token, url, HttpMethod.GET, null, responseType).getBody();
   }
 
+  private <T> T adminGet(String url, Class<T> responseType) {
+    return exchange(null, url, HttpMethod.GET, null, responseType).getBody();
+  }
+
   private <T> T post(SecurityToken token, String url, Object body, Class<T> responseType) {
     return exchange(token, url, HttpMethod.POST, body, responseType).getBody();
+  }
+
+  private <T> T adminPost(String url, Object body, Class<T> responseType) {
+    return exchange(null, url, HttpMethod.POST, body, responseType).getBody();
   }
 
   private <T> ResponseEntity<T> exchange(SecurityToken token, String url, HttpMethod method, Object request, Class<T> responseType) {
     RestTemplate restTemplate = new RestTemplate();
     HttpHeaders headers = new HttpHeaders();
     headers.setAccept(singletonList(MediaType.APPLICATION_JSON));
-    headers.set("session", token.getAdminToken());
-//    headers.set("admin", configService.getAdminPassword());
+
+    if (token != null) {
+      headers.set("session", token.getAdminToken());
+    } else {
+      headers.set("admin", configService.getAdminPassword());
+    }
 
     HttpEntity<Object> entity = new HttpEntity<>(request, headers);
 
