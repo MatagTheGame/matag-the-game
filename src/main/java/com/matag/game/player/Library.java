70ba64704a68ba9284d519ee50a33d3a6a628fb4
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-08-24 16:41:13 +0100

    Library does not need a mask method.

--- src/main/java/com/matag/game/player/Library.java
+++ src/main/java/com/matag/game/player/Library.java
@@ -1,29 +1,17 @@
 package com.matag.game.player;
 
 import com.matag.game.cardinstance.CardInstance;
-import com.matag.game.cardinstance.CardInstanceFactory;
 import com.matag.game.cardinstance.CardListComponent;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
-import java.util.List;
 
 @Component
 @Scope("prototype")
 public class Library extends CardListComponent {
-  private final CardInstanceFactory cardInstanceFactory;
-
-  public Library(CardInstanceFactory cardInstanceFactory) {
-    this.cardInstanceFactory = cardInstanceFactory;
-  }
-
   public CardInstance draw() {
     return this.cards.remove(0);
-  }
-
-  public List<CardInstance> maskedLibrary() {
-    return cardInstanceFactory.mask(this.cards);
   }
 
   public Library shuffle() {
